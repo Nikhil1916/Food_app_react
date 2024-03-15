6f54951133a2cf9565ca350dabdda26ba8c67494
nikhil <nikhilchawla9013@gmail.com>
2024-03-15 14:01:03 +0530

    added filteration

--- src/components/Body.js
+++ src/components/Body.js
@@ -9,15 +9,18 @@ const searchStyle = {
 
 export default Body = () => {
   const [restList , setRestList] = useState([]);
+  const [filteredRestList , setfilteredRestList] = useState([]);
+  const [searchVal , setSearchVal] = useState('');
+  
+  useEffect(()=>{
+    console.log("Use effect called");
+     fetchData();
+  }, []);
+  console.log("rerenderd body?");
+  
   const filterFnc = (val) => {
     return Number(val?.data?.avgRating)>=4;
   }
-
-  useEffect(()=>{
-    console.log("Use effect called");
-    fetchData();
-  }, []);
-
   const fetchData = async() => {
     const data = await fetch(
       "https://www.swiggy.com/dapi/restaurants/list/v5?lat=30.900965&lng=75.8572758&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
@@ -30,19 +33,34 @@ export default Body = () => {
       }
       return {data};
     }));
+    setfilteredRestList(json?.data?.cards?.[4]?.card?.card?.gridElements?.infoWithStyle?.restaurants?.map(r=>{
+      const data  = {
+        ...r?.info
+      }
+      return {data};
+    }));
   }
-
   return (
     <div className="body">
-      <div className="search" style={searchStyle}>
-        <button onClick={()=> {
+      <div className="filter" style={searchStyle}>
+      <div className="search">
+        <input placeholder="Search" type="text" className="search-box" value={searchVal} onChange={(e)=>{
+          setSearchVal(e?.target?.value);
+        }} /> 
+        <button className="search-btn" onClick={()=>{
+          const filteredRest = restList?.filter(d=>d?.data?.name?.toLowerCase()?.indexOf(searchVal?.toLocaleLowerCase()) > -1);
+          console.log(filteredRest);
+          setfilteredRestList(filteredRest);
+        }}>Search</button>
+      </div>
+        <button className="filter-btn" onClick={()=> {
           const filteredList = restList?.filter(filterFnc);
-          setRestList(filteredList)
-        } }>Filter 4 stars</button>
+          setfilteredRestList(filteredList)
+        } }>Top Rated Restaurants</button>
       </div>
       <div className="res-container">
       {
-        restList?.length > 0 ? restList.map((r)=>{
+        filteredRestList?.length > 0 ? filteredRestList.map((r)=>{
           return <RestaurantCard key={r?.data?.id} resData={r} />
         }) : <SchimmerCard count = {10} />
       }
