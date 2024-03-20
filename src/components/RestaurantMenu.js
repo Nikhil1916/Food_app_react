0680d05b24e61112e9250851333b5f6dc732d187
nikhil <nikhilchawla9013@gmail.com>
2024-03-20 16:53:37 +0530

    add menu list in accordian

--- src/components/RestaurantMenu.js
+++ src/components/RestaurantMenu.js
@@ -2,12 +2,13 @@ import { useEffect, useState } from "react";
 import SchimmerCard from "./SchimmerCard";
 import { useParams } from "react-router-dom";
 import { MENU_API } from "../utils/constant";
+import RestaurantMenuList from "./RestaurantMenuList";
 
 const RestaurantMenu = () => {
-
     const [ restInfo , setRestInfo ] = useState(null);
+    const [ restMenuList , setRestMenuList ] = useState([]);
+    const [ toggle, setToggle ] = useState(false);
     const {resId} = useParams();
-    // console.log(params);
     useEffect(()=>{
         fetchMenu();
     },[]);
@@ -15,30 +16,58 @@ const RestaurantMenu = () => {
     async function fetchMenu() {
         const data = await fetch(MENU_API + resId);
         const json = await data.json();
-        console.log(json);
         setRestInfo(json.data);
     }
     if(restInfo == null) {
         return <SchimmerCard count = {10} />
     }
-    console.log(restInfo?.cards?.[0]?.card?.card?.info);
+    const arr = restMenuList;
+    if(restMenuList?.length < 2){
+        const { itemCards , title } = restInfo?.cards?.[2]?.groupedCard?.cardGroupMap?.REGULAR?.cards?.[1]?.card?.card;
+        restMenuList?.length < 2 && arr.push(
+            {
+                itemCards,
+                title,
+                is_active: true
+            }
+        )
+    }
+
     const { name , cuisines , costForTwoMessage, city } = restInfo?.cards[0]?.card?.card?.info;
-    const { itemCards } = restInfo?.cards?.[2]?.groupedCard?.cardGroupMap?.REGULAR?.cards?.[1]?.card?.card;
-    console.log(restInfo);
-    return restInfo ===  null ? <SchimmerCard/> : (
+     if(restMenuList?.length < 2) {
+        const { itemCards , title} = restInfo?.cards?.[2]?.groupedCard?.cardGroupMap?.REGULAR?.cards?.[2]?.card?.card;
+         arr.push(
+            {
+                itemCards,
+                title,
+                is_active: false
+            }
+        )
+        setRestMenuList(arr);
+    }   return restInfo ===  null ? <SchimmerCard/> :
+     (
         <div className="rest-menu">
         <h1>{name}</h1>
         <p>{cuisines?.join(", ") } - {costForTwoMessage}</p>
         <p>{city}</p>
-        {/* <h3>{costForTwoMessage}</h3> */}
-        <h2>Menu</h2>
-        <ul>
-            {
-                itemCards?.map((data,i)=>{
-                    return <li style={{marginLeft:'2em', lineHeight:'2em'}} key={data?.card?.info?.id}>{data?.card?.info?.name} - {"Rs." + (data?.card?.info?.price/100 || data?.card?.info?.defaultPrice/100)}</li>
-                })
-            }
-        </ul>
+        {
+            restMenuList?.map((_)=>{
+                return (
+                    <div onClick={()=>{
+                        _.is_active = !_.is_active;
+                        console.log(_);
+                        setToggle(!toggle);
+                    }} className="accordion">
+                    <div className="accordion-item">
+                        <h2 className="accordion-title">{_.title}</h2>
+                        <div className={_.is_active? 'accordion-content' : 'd-none'}>
+                            <RestaurantMenuList  itemCards={_.itemCards} />
+                        </div>
+                    </div>
+                    </div>
+                )
+            })
+        }
         </div>
     )
 }
