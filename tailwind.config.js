d8b69982e8c947d6b3bce55ea56714c3c0b5836c
nikhil <nikhilchawla9013@gmail.com>
2024-04-05 00:19:04 +0530

    add tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  // it tells in which files can tailwinf be uses
+  content: ["./src/**/*.{html,js,ts,jsx,tsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
