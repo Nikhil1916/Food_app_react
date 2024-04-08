4eb16d4fd6e98c2ae2488092d320f0d3798fb8c6
nikhil <nikhilchawla9013@gmail.com>
2024-04-09 01:12:17 +0530

    added css changes

--- src/components/RestaurantMenu.js
+++ src/components/RestaurantMenu.js
@@ -9,13 +9,15 @@ const RestaurantMenu = () => {
   const { resId } = useParams();
   const restInfo = useRestaurantMenu(resId);
   if (restInfo == null) {
-    return <SchimmerAccordian count={10} />;
+    return <div className="flex justify-center"> <SchimmerAccordian count={10} /></div>;
   }
   const categories = restInfo?.cards?.[4]?.groupedCard?.cardGroupMap?.REGULAR?.cards?.filter(c=>c?.card?.card?.['@type'] == 'type.googleapis.com/swiggy.presentation.food.v2.ItemCategory')?.map(c=>c?.card?.card);
-  console.log(categories);
-
+  categories.forEach((cat, i)=>{
+    if(i==0) cat['is_active'] = true;
+    else cat['is_active'] = false
+  })
   const { name, cuisines, costForTwoMessage, city } =
-    restInfo?.cards?.[2]?.card?.card?.info;
+  restInfo?.cards?.[2]?.card?.card?.info;
 
   return restInfo === null ? (
     <SchimmerAccordian />
@@ -28,20 +30,23 @@ const RestaurantMenu = () => {
       <p className="font-semibold">{city}</p>
       {categories?.map((_,i) => {
         return (
-          <div className="flex justify-center" key={i}>
+          <div className="w-6/12 mx-auto" key={i}>
           {
           _?.itemCards?.length > 0 ?
              <div
               onClick={() => {
                 _.is_active = !_.is_active;
-                // console.log(_);
+                console.log(_);
                 setToggle(!toggle);
               }}
               className="accordion"  key={i}
             >
               <div className="accordion-item">
-                <h2 className="accordion-title">{_.title}</h2>
-                <div className={_.is_active ? "accordion-content" : "d-none"}>
+              <div className="bg-gray-50 p-4 shadow-lg my-4 flex justify-between items-center">
+                <h2 className="accordion-title font-bold">{_.title}  ({_?.itemCards?.length})</h2>
+                <span className="font-bold">^</span>
+              </div>
+                <div className={_.is_active ? "accordion-content block bg-gray-50 p-4 flex flex-col gap-2 font-normal " : "hidden"}>
                 <RestaurantMenuList itemCards={_.itemCards} />
                 </div>
               </div>
--- src/components/RestaurantMenuList.js
+++ src/components/RestaurantMenuList.js
@@ -1,4 +1,5 @@
 const RestaurantMenuList = (props) => {
+  console.log(props);
   const { itemCards } = props;
   return (
     <div>
@@ -6,13 +7,20 @@ const RestaurantMenuList = (props) => {
         {itemCards?.map((data, i) => {
           return (
             <li
-              style={{ marginLeft: "2em", lineHeight: "2em" }}
+              style={{ lineHeight: "2em" }}
               key={data?.card?.info?.id}
+              className="my-2 text-left border-b-2 py-2"
             >
-              {data?.card?.info?.name} -{" "}
+              <p className="m-1">{data?.card?.info?.name} -{" "}
               {"Rs." +
                 (data?.card?.info?.price / 100 ||
                   data?.card?.info?.defaultPrice / 100)}
+                  </p>
+                  <p className="text-xs text-gray-400">
+                    {
+                      data?.card?.info?.description
+                    }
+                  </p>
             </li>
           );
         })}
